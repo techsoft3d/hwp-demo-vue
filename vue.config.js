476b5e80feb1ab9ddcae8b25db4966b306220141
Jingsong Tan <jingsong.tan@techsoft3d.com>
2021-05-19 00:09:07 -0500

    Change title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
+  publicPath: '/hwp-demo-vue/',
 	devServer: {
     host: 'localhost'
-	}
+	},
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      title: 'HWP Vue Demo'
+    }
+  }
 }
